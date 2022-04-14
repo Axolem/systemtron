9d01c72856a34ee4fce4d4415764ff05ed133799
Axole Maranjana <axolemaranjana4@gmail.com>
2022-04-14 18:56:08 +0200

    Templets created

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # systemtron
 DSW Project
+
+Tab icon link: <a href="https://www.flaticon.com/free-icons/tab" title="tab icons">Tab icons created by Stockio - Flaticon</a>
--- config/header.php
+++ config/header.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Be Your Own Boss | Entrepreneurial Website</title>
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,700;1,200;1,300;1,400&display=swap" rel="stylesheet">
+    <!-- Bootstrap icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <!-- Tab Icon -->
+    <link rel="shortcut icon" href="../images/tabs.png" type="image/x-icon">
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="../css/dash.css">
+    <link rel="stylesheet" href="../css/profiles.css">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
 <!-- Do not add any content below this line. -->
