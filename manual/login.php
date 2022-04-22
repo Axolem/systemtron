c95d3668e9f2cc57c20d322b847496ba09bc5fe6
Axole Maranjana <axolemaranjana4@gmail.com>
2022-04-22 19:22:35 +0200

    CSS - Made login page responsive PHP - Created login full functionality

--- manual/login.php
+++ manual/login.php
@@ -1,46 +1,46 @@
-<?php 
+<?php
 
 session_start();
 
-	include("connection.php");
-	include("functions.php");
+include("connection.php");
+include("functions.php");
 
 
-	if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		//something was posted
-		$user_name = $_POST['user_name'];
-		$password = md5($_POST['password']);
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	//something was posted
+	$email =  test_input($_POST['email']);
+	$password =  test_input(md5($_POST['password']));
 
-		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
-		{
+	if (!empty($email) && !empty($password)) {
 
-			//read from database
-			$query = "select * from users where email = '$user_name' limit 1";
-			$result = mysqli_query($con, $query);
+		//read from database
+		$query = "select * from users where email = '$email' limit 1";
+		$result = mysqli_query($con, $query);
 
-			if($result)
-			{
-				if($result && mysqli_num_rows($result) > 0)
-				{
+		if ($result) {
+			if ($result && mysqli_num_rows($result) > 0) {
+				$user_data = mysqli_fetch_assoc($result);
 
-					$user_data = mysqli_fetch_assoc($result);
-					
-					if($user_data['password'] === $password)
-					{
-
-						$_SESSION['user_id'] = $user_data['user_id'];
-						header("Location: index.php");
+				if ($user_data['oauth_provider'] == "manual") {
+					if ($user_data['oauth_id'] === $password) {
+						$_SESSION['email'] = $email;
+						header("Location: ../dashboard/index.php");
+						die;
+					} else {
+						header("Location: http://localhost/project/systemtron/login.php?loginErr=Incorrect password or email.");
 						die;
 					}
+				} else {
+					header("Location: http://localhost/project/systemtron/login.php?loginErr=Try to login with Google or Facebook");
+					die;
 				}
 			}
-			
-			echo $password;
-		}else
-		{
-			echo $password;
+		} else {
+			header("Location: http://localhost/project/systemtron/login.php?loginErr=User not registers");
+			die;
 		}
+	} else {
+		header("Location: http://localhost/project/systemtron/login.php?loginErr=Please input all fields");
+		die;
 	}
-
-?>
+}
