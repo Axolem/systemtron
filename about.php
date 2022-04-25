82bac5057975ce442d098f4e20ae545809d1647b
MichelleNdzu <103642123+MichelleNdzu@users.noreply.github.com>
2022-04-25 15:27:34 +0200

    resized a pic

--- about.php
+++ about.php
@@ -4,6 +4,39 @@ include('config/navbar.php'); ?>
 
     <!-- You code goes here. -->
 
+<div span="us">
+<h2>ABOUT US</h2>
+<br>
+    <p>
+        Be Your Own Boss is a national B2C company. The company mainly focuses on helping individuals who are over 18 with educational background to start their own businesses.
+        Be Your Own Boss mainly targets South African people. The brand was founded in 2022, and since then it has been upholding the philosophy that 
+        "everyone can start their businesses and fight poverty". The business covers all the nine provinces of South Africa.
+    </p>
+    <br>
+    <h2>OUR MISSION</h2>
+    <br>
+    <p>
+        Be Your Own Boss prides itself on offering business ideas and funds to start a business of your choice. It adheres to the concept that "everyone can run a business".
+        Be Your Own Boss is able to stay on top while bringing these opportunities to people. It aims to promptly offer outstanding ideas to those that meet the requirements. 
+    </p>
+    <br>
+    <h2>WHERE YOU CAN FIND US</h2>
+    <br>
+    <p>
+        Our head offices are in Gauteng Johannesburg. But we can also help in any ways online unless if there is something that will need the person to be present.
+    </p>
+    <img class="image" src="mapppp.png" alt="">
+    <br>
+    <br>
+    <h2>CUSTOMER SERVICE</h2>
+    <br>
+    <p>
+        You may have questions but we have got you covered. Our team of customer service professionals can communicate with you in the following languages:
+        English, Xitsonga, isiZulu, Sesotho and Tshivenda.
+    </p>
+    <img src="receptionist.png" alt="">
+</div>
+
 
 
 <!-- This inserts the footer -->
