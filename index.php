0eabd751d2804f0ed62f341587b112ef43d0d166
junior-03 <juniormaruping6@gmail.com>
2022-04-26 22:50:25 +0200

    homepage

--- index.php
+++ index.php
@@ -24,9 +24,35 @@ include('config/navbar.php'); ?>
       <li><img src="images/growth.jpg" alt="bob-growth"></li>
       <li><img src="images/growth2.jpg" alt="bob-growth2"></li>
     </ul>
+    <hr size="2"width="90%" color="grey">
+    <section class="home">
+     <p>We pride ourselves in providing excellence through our programs.
+    Over 90% of our students have completed our modules and stand a greater chance to be great entreprenenurs with successful businesses.
+      We first start by encouraging students to engage with thier peers and think creatively and spot opportunities
+      for your own development.Youll learn to trust your judgement ,whatever the world throws at you .   
+      </p>
+      <div class="home2">
+      <p>
+        Be Your Own Boss offers exceptional flexibility providing you with excellent programs that are suitable
+        for your personal life.We have a dedicated team that provides easily accesible support that will help 
+        you along your journey.Our programs are aimed at building students and giving them a strong foundation in the 
+        essential skills required for a successful entreprenenurship journey.
+      </p>
+    </div>
+    </section>
+    <ul class="imsh">
+      <li><img src="images/creativ.jpg" alt="bob-creativ"></li>
+      <li><img src="images/creative2.jpg" alt="bob-creative2"></li>
+    </ul>
+    <hr size="2"width="90%" color="grey">
+    <div class="home3">
+      <p><h2>Great Environment , fruitful results </h2></p>
+      <p>BOB is diverse and skillfil enough to help anyone of any age.Once in youll never regret it.</p>
+      <li><a href=" ">Learn More</a></li>
+    </div>
   </main>
   <section class="register">
-    <h2>Register to Join Today!</h2>
+    <h2>Sign Up to Join Today!</h2>
     <p>From small to big and many more youll discover a effortless way to stay on top of the game.</p>
     <form>
       <input type="email" name="email" placeholder="Login" required>
