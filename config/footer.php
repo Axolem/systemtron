3171c64fc0216c811209626f5f7ccb1fa871cd80
NesleyB <103641828+NesleyB@users.noreply.github.com>
2022-04-27 11:35:20 +0200

    ,

--- config/footer.php
+++ config/footer.php
@@ -1,26 +1,24 @@
+<link rel="stylesheet" href="http://localhost/project/systemtron/css/styles.css">
 <footer>
-
 <div class="container">
-    <div class ="w">
-        <h4>BOB</h4>
+    <div class="w">
+        <h4 class="Nes">BOB</h4>
         <div class="social-links">
         <a href="facebook"><i class="bi bi-facebook"></i></a>
         <a href="twitter"><i class="bi bi-twitter"></i></a>
         <a href="instagram"><i class="bi bi-instagram"></i></a>
         <a href="Be your own boss" 18 Falcon Street Auckland Park Johannesburg ><i class="bbi bi-geo-alt"></i></a>
     </div>
-</div>
-<div>
-        <ul id="more-info">
-            
+    <div class="container">
+        <ul class="more-info">  
             <li><a href="/SUPPORT">SUPPORT</a></li>
             <li><a href="BLOG">BLOG</a>
             <li><a href="/Privacy Policy/Terms of use">privacy policy and Terms of use</a></li><br>
             <li><a href="/About Us">About BOB</a></li>
             <li><a href="/FAQ">FAQ</a>
             <li><a href="Apply">Apply</a>
-            
         </ul>
+    </div>
 </div>
 <p> &copy <?php echo date('Y')?> Be your own boss</p>
 
