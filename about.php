7f03edf98e98c1544ef3a37d23848423243ce1af
MichelleNdzu <103642123+MichelleNdzu@users.noreply.github.com>
2022-04-27 23:20:44 +0200

    added images

--- about.php
+++ about.php
@@ -2,16 +2,17 @@
 include('config/navbar.php'); ?>
 
 
-    <!-- You code goes here. -->
+    <!-- Your code goes here. -->
 
-<div span="us">
-<h2>ABOUT US</h2>
+<div id="us">
+<h2 class=heads>ABOUT US</h2>
 <br>
     <p>
         Be Your Own Boss is a national B2C company. The company mainly focuses on helping individuals who are over 18 with educational background to start their own businesses.
         Be Your Own Boss mainly targets South African people. The brand was founded in 2022, and since then it has been upholding the philosophy that 
         "everyone can start their businesses and fight poverty". The business covers all the nine provinces of South Africa.
     </p>
+    <img src="images/about.jfif" alt="">
     <br>
     <h2>OUR MISSION</h2>
     <br>
@@ -19,6 +20,7 @@ include('config/navbar.php'); ?>
         Be Your Own Boss prides itself on offering business ideas and funds to start a business of your choice. It adheres to the concept that "everyone can run a business".
         Be Your Own Boss is able to stay on top while bringing these opportunities to people. It aims to promptly offer outstanding ideas to those that meet the requirements. 
     </p>
+    <img src="images/mission.jfif" alt="">
     <br>
     <h2>WHERE YOU CAN FIND US</h2>
     <br>
