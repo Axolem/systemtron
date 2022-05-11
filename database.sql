4901f6c2fe69d33cdfe77206e0544c47d1929126
Axole Maranjana <axolemaranjana4@gmail.com>
2022-05-11 18:15:26 +0200

    Updated databas

--- database.sql
+++ database.sql
@@ -1,46 +1,54 @@
-CREATE TABLE blog (
-  id        int(11) NOT NULL, 
-  `date`    varchar(25), 
-  category  varchar(25) NOT NULL, 
-  headline  varchar(255) NOT NULL, 
-  story     longtext, 
-  tab       varchar(25) NOT NULL, 
-  picture   varchar(50) NOT NULL, 
-  teamid_no int(11) NOT NULL, 
-  PRIMARY KEY (id), 
-  INDEX (id));
+CREATE DATABASE beyourownboss;
 
-CREATE TABLE subscribers (
-  id    int(11) NOT NULL, 
-  name  varchar(25) NOT NULL, 
-  email varchar(25) NOT NULL UNIQUE, 
+USE beyourownboss;
+
+CREATE TABLE newsletters (
+  id       int(11) NOT NULL, 
+  email_id varchar(255) NOT NULL UNIQUE, 
+  name     varchar(25) NOT NULL, 
+  email    int(11) NOT NULL, 
   PRIMARY KEY (id, 
-  email), 
-  INDEX (id));
+  email_id), 
+  UNIQUE INDEX (id));
 
 CREATE TABLE team (
-  name          varchar(255) NOT NULL, 
-  id_no         int(11) NOT NULL AUTO_INCREMENT, 
-  course        varchar(255) NOT NULL, 
-  paragraph_one longtext NOT NULL, 
-  paragraph_two longtext NOT NULL, 
-  PRIMARY KEY (id_no), 
-  UNIQUE INDEX (id_no));
+  id         int(11) NOT NULL, 
+  name       varchar(25) NOT NULL, 
+  surname    varchar(25) NOT NULL, 
+  email      varchar(100) NOT NULL UNIQUE, 
+  student_no int(11) NOT NULL, 
+  PRIMARY KEY (id, 
+  email, 
+  student_no), 
+  UNIQUE INDEX (id));
+
+CREATE TABLE user_details (
+  usersemail varchar(50) NOT NULL, 
+  first_name varchar(25) NOT NULL, 
+  last_name  varchar(25) NOT NULL, 
+  gender     varchar(20), 
+  ethinicity varchar(20), 
+  picture    varchar(255), 
+  phone      varchar(20) UNIQUE, 
+  emp_status varchar(25), 
+  PRIMARY KEY (usersemail));
+
+CREATE TABLE user_setting (
+  usersemail          varchar(50) NOT NULL, 
+  phone_notifications varchar(3) DEFAULT 'yes' NOT NULL, 
+  email_notifications varchar(25) DEFAULT 'yes' NOT NULL, 
+  PRIMARY KEY (usersemail));
 
 CREATE TABLE users (
-`id` int(11) NOT NULL PRIMARY KEY,
-  `oauth_provider` varchar(50) NOT NULL,
-  `oauth_id` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL UNIQUE,
-  `picture` varchar(255) ,
-  `created_at` varchar(50) ,
-  `modified_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(), 
-  ethnic_group   varchar(15), 
-  emplopment_status    varchar(255), 
-  phone varchar(13) UNIQUE,
-  gender varchar(25),
-  code int(11) NULL,
-  verified char(3) NULL);
-
+  id            int(11) NOT NULL UNIQUE, 
+  oath_provider varchar(10) NOT NULL, 
+  oath_id       varchar(255) NOT NULL, 
+  email         varchar(50) NOT NULL, 
+  created       timestamp NOT NULL, 
+  updated       timestamp NULL, 
+  code          int(4), 
+  verified      char(3) NOT NULL, 
+  PRIMARY KEY (email));
+  
+ALTER TABLE user_setting ADD CONSTRAINT FKuser_setti76755 FOREIGN KEY (usersemail) REFERENCES users (email) ON UPDATE Cascade ON DELETE Cascade;
+ALTER TABLE user_details ADD CONSTRAINT FKuser_detai856920 FOREIGN KEY (usersemail) REFERENCES users (email) ON UPDATE Cascade ON DELETE Cascade;
