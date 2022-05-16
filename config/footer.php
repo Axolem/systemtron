5a55353608a84ed36fa57102c8835709c7c125cd
Axole Maranjana <axolemaranjana4@gmail.com>
2022-05-16 19:17:46 +0200

    added responsiveness

--- config/footer.php
+++ config/footer.php
@@ -1,33 +1,37 @@
-<link rel="stylesheet" href="http://localhost/project/systemtron/css/styles.css">
+<span class="underline"></span>
 <footer>
-<div class="container">
-    <div class="w">
-        <h4 class="Nes">BOB</h4>
-        <div class="social-links">
-        <a href="facebook"><i class="bi bi-facebook"></i></a>
-        <a href="twitter"><i class="bi bi-twitter"></i></a>
-        <a href="instagram"><i class="bi bi-instagram"></i></a>
-        <a href="Be your own boss" 18 Falcon Street Auckland Park Johannesburg ><i class="bbi bi-geo-alt"></i></a>
-    </div>
-    <div class="container">
-        <ul class="more-info">  
-            <li><a href="/SUPPORT">SUPPORT</a></li>
-            <li><a href="BLOG">BLOG</a>
-            <li><a href="/Privacy Policy/Terms of use">privacy policy and Terms of use</a></li><br>
-            <li><a href="/About Us">About BOB</a></li>
-            <li><a href="/FAQ">FAQ</a>
-            <li><a href="Apply">Apply</a>
-        </ul>
-    </div>
-</div>
-<p> &copy <?php echo date('Y')?> Be your own boss</p>
+    <div class="footer-container">
+        <div class="footer-link-wrapper">
+            <a href="http://localhost/project/systemtron/index.php" class="footer-h4">BOB</a>
+            <div class="social-links">
+                <a href=""><i class="bi bi-facebook"></i></a> <span class="separator"></span>
+                <a href=""><i class="bi bi-twitter"></i></a><span class="separator"></span>
+                <a href=""><i class="bi bi-instagram"></i></a><span class="separator"></span>
+                <a href=""><i class="bi bi-geo-alt"></i></a>
+            </div>
+            <div class="footer-page-links">
+                <ul class="row">
+                    <li><a href="http://localhost/project/systemtron/">home</a></li>
+                    <li><a href="http://localhost/project/systemtron/about.php">about us</a></li>
+                    <li><a href="/Privacy Policy/Terms of use">privacy policy</a></li>
+                    <li><a href="http://localhost/project/systemtron/blog.php">blog</a></li>
+                    <li><a href="http://localhost/project/systemtron/login.php">login</a></li>
+                    <li><a href="http://localhost/project/systemtron/contact.php">contact us</a></li>
+                    <li><a href="http://localhost/project/systemtron/team.php">team</a></li>
+                </ul>
+            </div>
+        </div>
+        
+        <p> &copy <?php echo date('Y') ?> Be your own boss</p>
 
-<P>views and opinions expressed in each episode are those of the contributors and do not necessarily
-reflect the official policy or position of the agency.You are worthy for your work ,you are worthy of doing something great 
-and if you can just hang on a little longer you're going to be just fine.Be your own boss ,if you dont build your dream 
-someone will hire you to build theirs.</P>
-
+        <P>views and opinions expressed in each episode are those of the contributors and do not necessarily
+            reflect the official policy or position of the agency.You are worthy for your work ,you are worthy of doing something great
+            and if you can just hang on a little longer you're going to be just fine.Be your own boss ,if you dont build your dream
+            someone will hire you to build theirs.
+        </P>
+    </div>
 </footer>
 <script src="js/nav.js"></script>
 </body>
+
 </html>
