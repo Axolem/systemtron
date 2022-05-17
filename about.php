aa6277b989a13933784881a2b7ca1141fe94128a
Axole Maranjana <axolemaranjana4@gmail.com>
2022-05-17 17:53:18 +0200

    Made fully responsive

--- about.php
+++ about.php
@@ -4,50 +4,72 @@ include('config/navbar.php'); ?>
 
 <!-- Your code goes here. -->
 
-<div id="us">
-<h2 class="heads">ABOUT US</h2>
-<div class="row">
-    <p class="mine">
-        Be Your Own Boss is a national B2C company. The company mainly focuses on helping individuals who are over 18 with educational background to start their own businesses.
-        Be Your Own Boss mainly targets South African people. The brand was founded in 2022, and since then it has been upholding the philosophy that
-        "everyone can start their businesses and fight poverty". The business covers all the nine provinces of South Africa.
-    </p>
-    <img class="about-image" src="images/about.jfif" alt="">
-</div>
-    
+<div class="about-container">
+    <div class="about-wrapper">
 
-
-    <h2  class="heads">OUR MISSION</h2>
-    <div class="row">
-        <p class="mine">
-        Be Your Own Boss prides itself on offering business ideas and funds to start a business of your choice. It adheres to the concept that "everyone can run a business".
-        Be Your Own Boss is able to stay on top while bringing these opportunities to people. It aims to promptly offer outstanding ideas to those that meet the requirements.
-    </p>
-
-    <img class="about-image" src="images/mission.jfif" alt="">
-    </div>
-    
-
-
-    <h2 class="heads">WHERE YOU CAN FIND US</h2>
-    <div class="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3580.241453914132!2d28.013953514982486!3d-26.18882268344465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e950bf441bb28e7%3A0xa2f3fb63313a8e59!2sUJ%20Bunting%20Road%20Campus!5e0!3m2!1sen!2sza!4v1651498850826!5m2!1sen!2sza" 
-        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div class="about-content">
+            <h2 class="contact-h2">ABOUT US</h2>
+            <div class="row">
+                <div class="inner-row">
+                    <img class="about-image" src="images/about.jfif" alt="">
+                </div>
+                    <div class="inner-row">
+                        <p class="mine">
+                            Be Your Own Boss is a national B2C company. The company mainly focuses on helping individuals who are over 18 with educational background to start their own businesses.
+                            Be Your Own Boss mainly targets South African people. The brand was founded in 2022, and since then it has been upholding the philosophy that
+                            "everyone can start their businesses and fight poverty". The business covers all the nine provinces of South Africa.
+                        </p>
+                    </div>
+            </div>
         </div>
-
-    <h2  class="heads">CUSTOMER SERVICE</h2>
-    <div class="row about-row">
-        <p class="mine">
-        You may have questions but we have got you covered. Our team of customer service professionals can communicate with you in the following languages:
-        English, Xitsonga, isiZulu, and Sesotho. Be Your Own Boss is a national managed hosting provider, serving over 1,000 customers of varied sizes.
-        The company strongly focuses on the commitment to their customer experience strategy.
-    </p>
-
-    <img class="about-image" src="images/receptionist.png" alt="">
     </div>
-    
-</div>
 
+    <div class="about-wrapper">
+
+        <div class="about-content">
+            <h2 class="contact-h2">OUR MISSION</h2>
+            <div class="row">
+                <div class="inner-row">
+                    <p class="mine">
+                        Be Your Own Boss prides itself on offering business ideas and funds to start a business of your choice. It adheres to the concept that "everyone can run a business".
+                        Be Your Own Boss is able to stay on top while bringing these opportunities to people. It aims to promptly offer outstanding ideas to those that meet the requirements.
+                    </p>
+                </div>
+                    <div class="inner-row">
+                        <img class="about-image" src="images/mission.jfif" alt="">
+                    </div>
+            </div>
+        </div>
+    </div>
+    <div class="about-wrapper">
+
+        <div class="about-content">
+            <h2 class="contact-h2">OUR VALUES</h2>
+            <div class="row">
+                <div class="inner-row">
+                    <p class="mine">
+                        You may have questions but we have got you covered. Our team of customer service professionals can communicate with you in the following languages:
+                        English, Xitsonga, isiZulu, and Sesotho. Be Your Own Boss is a national managed hosting provider, serving over 1,000 customers of varied sizes.
+                        The company strongly focuses on the commitment to their customer experience strategy.
+                    </p>
+                </div>
+                <div class="inner-row">
+                    <img class="about-image" src="images/receptionist.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="about-wrapper">
+
+        <div class="about-content">
+            <h2 class="contact-h2">WHERE YOU CAN FIND US</h2>
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3580.241453914132!2d28.013953514982486!3d-26.18882268344465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e950bf441bb28e7%3A0xa2f3fb63313a8e59!2sUJ%20Bunting%20Road%20Campus!5e0!3m2!1sen!2sza!4v1651498850826!5m2!1sen!2sza" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <!-- This inserts the footer -->
