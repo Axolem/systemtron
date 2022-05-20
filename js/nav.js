ccabbd81d360c0282550898690b0ba6f13747904
Axole Maranjana <axolemaranjana4@gmail.com>
2022-05-20 14:09:22 +0200

    Made responsive and made tabed loging/registration

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,23 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
 
+//Login JS
+var loginBtn = document.getElementById('loginTab');
+var registerBtn = document.getElementById('registerTab');
+var z = document.getElementById('btn');
+
+function register() {
+    loginBtn.style.left = '-300px';
+    registerBtn.style.left = '40px';
+    z.style.left = '110px';
+}
+
+function login() {
+    loginBtn.style.left = '60px';
+    registerBtn.style.left = '500px';
+    z.style.left = '0px';
+}
+
 //Blog JS
 let toggles = document.getElementsByClassName('toggle');
 let conentDiv = document.getElementsByClassName('blog-cards');
--- login.php
+++ login.php
@@ -2,26 +2,27 @@
 include('config/navbar.php'); ?>
 
 <main>
-    <div class="login-container">
-        <div class="row login-row">
-            <div class="col login-col">
-                <h2>
-                    <center>LOGIN</center>
-                </h2>
-                <?php if (!empty($errors)) {
-                    echo "<p class='error'> <!--this is where the errors will be displayed--></p> ";
-                } ?>
+    <div id='login-form' class='login-page'>
+        <div class="form-box">
+            <div class='button-box'>
+                <div id='btn'></div>
+                <button type='button' onclick='login()' class='toggle-btn'>Log In</button>
+                <button type='button' onclick='register()' class='toggle-btn'>Register</button>
+            </div>
+            <div id='loginTab' class='input-group-login'>
                 <form class="login-form" action="manual/login.php" method="post">
-                    <label for="email">Email:</label>
-                    <input type="email" name="email" required value=<?php if (!empty($_GET['email'])) {echo $_GET['email'];} ?>>
+                    <label class="no-margin" for="email">Email:</label>
+                    <input type="email" name="email" required value=<?php if (!empty($_GET['email'])) {
+                                                                        echo $_GET['email'];
+                                                                    } ?>>
                     <label for="password">Password:</label>
                     <input type="password" name="password" required>
                     <a class="small-link" href="">Forgot your password?</a>
-                    <?php 
-                        
-                        if (!empty($_GET['loginErr'])) {
-                            echo "<p class='error'>".$_GET['loginErr']."</p> ";
-                        } ?>
+                    <?php
+
+                    if (!empty($_GET['loginErr'])) {
+                        echo "<p class='error'>" . $_GET['loginErr'] . "</p> ";
+                    } ?>
                     <button class="login-btn btn" type="submit">Login</button>
                 </form>
                 <div class="fb-goog">
@@ -41,84 +42,91 @@ include('config/navbar.php'); ?>
                     <p>*By logging in, you can access more educational content, view and learn more about
                         our partners and connect with other Entrepreneurs.</p>
                 </div>
-            </div>
-            <div class="col register-col">
-                <h2>
-                    <center>REGISTER</center>
-                </h2>
-                <form class="reg-form" action="manual/signup.php" method="post">
-                    <div class="reg-container">
-                        <div class="row">
-                            <div class="col reg-col">
-                                <label for="fname">First name:</label>
-                                <input type="text" name="fname" id="" required value=<?php if (!empty($_GET['name'])) {echo $_GET['name'];} ?>>
-                            </div>
-                            <div class="col reg-col">
-                                <label for="lname">Last name:</label>
-                                <input type="text" name="lname" required value=<?php if (!empty($_GET['lname'])) {echo $_GET['lname'];} ?>>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col reg-col">
-                                <label for="email">Email:</label>
-                                <input type="email" name="email" required value=<?php if (!empty($_GET['email'])) {echo $_GET['email'];} ?>>
-                            </div>
-                            <div class="col reg-col">
-                                <label for="email">Confirm email:</label>
-                                <input type="email" name="confirmEmail" required value=<?php if (!empty($_GET['cemail'])) {echo $_GET['cemail'];} ?>>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col reg-col">
-                                <label for="color">Ethnic group:</label>
-                                <select name="color"required value=<?php if (!empty($_GET['color'])) {echo $_GET['color'];} ?>>
-                                    <option value="african">African</option>
-                                    <option value="coloured">Coloured</option>
-                                    <option value="white">White</option>
-                                    <option value="indian">Indian</option>
-                                    <option value="other">Prefer not to say</option>
-                                </select>
-                            </div>
-                            <div class="col reg-col">
-                                <label for="gender">Gender group:</label>
-                                <select name="gender" required value=<?php if (!empty($_GET['gender'])) {echo $_GET['gender'];} ?>>
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                                    <option value="other">Prefer not to say</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col reg-col">
-                                <label for="password">Password:</label>
-                                <input type="password" name="password" required>
-                            </div>
-                            <div class="col reg-col">
-                                <label for="password">Password:</label>
-                                <input type="password" name="confirmPassword" required>
-                            </div>
-                        </div>
-                        <div class="privacy">
-                            <input type="checkbox" name="agree" required>
-                            <label for="agree">I agree to the BOB <a href="">Privacy Policy </a>and<a href=""> Terms of Use</a> .</label>
-                        </div>
 
-                        <?php 
-                        
-                        if (!empty($_GET['inputErr'])) {
-                            echo "<p class='error'>".$_GET['inputErr']."</p> ";
-                        } ?>
-                        <button class="btn reg-btn" type="submit">Register</button>
+            </div>
+            <form id='registerTab' class='input-group-register' action="manual/signup.php" method="post">
+                <div class="reg-container">
+                    <div class="row">
+                        <div class="col reg-col">
+                            <label class="no-margin" for="fname">First name:</label>
+                            <input type="text" name="fname" id="" required value=<?php if (!empty($_GET['name'])) {
+                                                                                        echo $_GET['name'];
+                                                                                    } ?>>
+                        </div>
+                        <div class="col reg-col">
+                            <label class="no-margin" for="lname">Last name:</label>
+                            <input type="text" name="lname" required value=<?php if (!empty($_GET['lname'])) {
+                                                                                echo $_GET['lname'];
+                                                                            } ?>>
+                        </div>
                     </div>
-                </form>
-            </div>
+                    <div class="row">
+                        <div class="col reg-col">
+                            <label for="email">Email:</label>
+                            <input type="email" name="email" required value=<?php if (!empty($_GET['email'])) {
+                                                                                echo $_GET['email'];
+                                                                            } ?>>
+                        </div>
+                        <div class="col reg-col">
+                            <label for="email">Confirm email:</label>
+                            <input type="email" name="confirmEmail" required value=<?php if (!empty($_GET['cemail'])) {
+                                                                                        echo $_GET['cemail'];
+                                                                                    } ?>>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col reg-col">
+                            <label for="color">Ethnic group:</label>
+                            <select name="color" required value=<?php if (!empty($_GET['color'])) {
+                                                                    echo $_GET['color'];
+                                                                } ?>>
+                                <option value="african">African</option>
+                                <option value="coloured">Coloured</option>
+                                <option value="white">White</option>
+                                <option value="indian">Indian</option>
+                                <option value="other">Prefer not to say</option>
+                            </select>
+                        </div>
+                        <div class="col reg-col">
+                            <label for="gender">Gender group:</label>
+                            <select name="gender" required value=<?php if (!empty($_GET['gender'])) {
+                                                                        echo $_GET['gender'];
+                                                                    } ?>>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                                <option value="other">Prefer not to say</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col reg-col">
+                            <label for="password">Password:</label>
+                            <input type="password" name="password" required>
+                        </div>
+                        <div class="col reg-col">
+                            <label for="password">Password:</label>
+                            <input type="password" name="confirmPassword" required>
+                        </div>
+                    </div>
+                    <div class="privacy">
+                        <input type="checkbox" name="agree" required>
+                        <label for="agree">I agree to the BOB <a href="">Privacy Policy </a>and<a href=""> Terms of Use</a> .</label>
+                    </div>
+
+                    <?php
+
+                    if (!empty($_GET['inputErr'])) {
+                        echo "<p class='error'>" . $_GET['inputErr'] . "</p> ";
+                    } ?>
+                    <button class="btn reg-btn" type="submit">Register</button>
+                </div>
+            </form>
         </div>
-
     </div>
+
+
+
 </main>
-
-
-
 
 <!-- This inserts the footer -->
 <?php include('config/footer.php'); ?>
