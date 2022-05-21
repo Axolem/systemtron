2d3f74512b19647192ac00ddc7b9032ada0ec1f8
Axole Maranjana <axolemaranjana4@gmail.com>
2022-05-22 01:57:23 +0200

    Removed excess code

--- about2.php
+++ about2.php
@@ -1,18 +1,7 @@
-<?php include('config/header.php'); 
+<?php include('config/header.php');
 include('config/navbar.php'); ?>
- <link rel="stylesheet" href="http://localhost/project/systemtron/css/profiles.css">
- 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    
-</head>
-<body>
-    <!-----Your code goes here--->
+
+<!-----Your code goes here--->
 <h2>Hey!! Future Business Man/Women.</h2>
 <p><b> Here's a little insight of who the creators of this page are.</b></p>
 <br></br>
@@ -20,28 +9,23 @@ include('config/navbar.php'); ?>
 <div class="line">
   <div class="cube">
     <div class="square">
-        <!---your profile picture----> 
+      <!---your profile picture---->
       <img src="mypic.jpg" alt="my profile picture" style="width:100%">
       <div class="section">
-          <!----Self Description---->
+        <!----Self Description---->
         <b>Thatohatsi Gadipedi</b>
         <p class="heading">Supporting Designer </p>
         <p>I am Thatohatsi Gadipedi and I am part of this wonderful team that created and designed this very helpful website to assist people like you who only want to know how to become succesful enterpreneurs.</p>
-        <p>I am a second year Business Information Technology student at the University of Johannesburg and I aspire to become a  Business Analyst so I can help more business owners on the ventures</p>
-        <!----Your social Media handles---->   
+        <p>I am a second year Business Information Technology student at the University of Johannesburg and I aspire to become a Business Analyst so I can help more business owners on the ventures</p>
+        <!----Your social Media handles---->
         <p><i class="bi bi-facebook" style="color:blue;"></i> Thatohatsi Gadipedi </p>
         <p><i class="bi bi-twitter" style="color:blue;"></i> Thato_Gbaby </p>
         <p><i class="bi bi-instagram" style="color:purple;"></i> @theReal_Gbaby</p>
       </div>
     </div>
   </div>
-  
-
- <!-- This inserts the footer -->
-<?php include('config/footer.php') ?>
-<!-- Do not add any content below this line. -->   
-</body>
-</html>
 
 
-
+  <!-- This inserts the footer -->
+  <?php include('config/footer.php') ?>
+  <!-- Do not add any content below this line. -->
